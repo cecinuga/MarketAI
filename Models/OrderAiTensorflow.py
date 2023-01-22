009d5715134ad368778baab873c84807257eae10
Peter Bruegel <masterammo01@gmail.com>
2023-01-22 23:25:01 +0100

    gg

--- Models/OrderAiTensorflow.py
+++ Models/OrderAiTensorflow.py
@@ -17,45 +17,40 @@ class MLPLinearRegressor(object):
 
     
 
-        def initialize(self, X, y):
-            n_samples, n_features = X.shape
-            n_output = y.shape[0]
-            self.activation = {}
-            normal_initiliazer = tf.random_normal_initializer(seed=0, mean=0.0, stddev=1.0)
-            XN = tf.keras.utils.normalize(X)
-            self.X = tf.constant(XN, name="X", dtype=tf.double)
-            self.y = tf.constant(np.array(y).reshape(-1, 1), name="y", dtype=tf.double)
+    def initialize(self, X, y):
+        n_samples, n_features = X.shape
+        n_output = y.shape[0]
+        self.activation = {}
+        normal_initiliazer = tf.random_normal_initializer(seed=0, mean=0.0, stddev=1.0)
+        XN = tf.keras.utils.normalize(X)
+        self.X = tf.constant(XN, name="X", dtype=tf.double)
+        self.y = tf.constant(np.array(y).reshape(-1, 1), name="y", dtype=tf.double)
 
-            self.f_weights = tf.Variable(normal_initiliazer([self.n_neurons, n_samples, n_features], dtype=tf.double), name="f_weights", dtype=tf.double, trainable=True)
-            self.h_weights = tf.Variable(normal_initiliazer([self.n_layers, self.n_neurons, n_samples, n_output], dtype=tf.double), name="h_weights", dtype=tf.double, trainable=True)
-            self.bias = tf.Variable([0.01 for l in range(self.n_layers)], name="bias", dtype=tf.double, trainable=True)
-            
-            self.activation = tf.TensorArray(tf.double, size=0, dynamic_size=True, clear_after_read=False, tensor_array_name="Activation_Layers")
-            self.activation.write(0, [tf.math.sigmoid(tf.matmul(X, tf.transpose(self.f_weights[n])) + self.bias[0]) for n in range(self.n_neurons)]).mark_used()
-            for l in range(1,self.n_layers-1):
-                self.activation.write(l, [tf.math.sigmoid(tf.matmul(np.array(self.activation.read(l-1)[n]), self.h_weights[l][n]) + self.bias[l]) for n in range(self.n_neurons)])
-            self.activation.write(self.n_layers-1, [tf.math.sigmoid(tf.matmul(self.activation.read(self.n_layers-2)[n], self.h_weights[self.n_layers-1][n]) + self.bias[self.n_layers-1]) for n in range(self.n_neurons)])
+        self.f_weights = tf.Variable(normal_initiliazer([self.n_neurons, n_samples, n_features], dtype=tf.double), name="f_weights", dtype=tf.double, trainable=True )
+        self.h_weights = tf.Variable(normal_initiliazer([self.n_layers, self.n_neurons, n_samples, n_output], dtype=tf.double), name="h_weights", dtype=tf.double, trainable=True )
+        self.bias = tf.Variable([0.01 for l in range(self.n_layers)], name="bias", dtype=tf.double, trainable=True )
+        
+        self.activation = tf.TensorArray(tf.double, size=0, dynamic_size=True, clear_after_read=False, tensor_array_name="Activation_Layers")
+        self.activation.write(0, [tf.math.sigmoid(tf.matmul(X, tf.transpose(self.f_weights[n])) + self.bias[0]) for n in range(self.n_neurons)]).mark_used()
+        for l in range(1,self.n_layers-1):
+            self.activation.write(l, [tf.math.sigmoid(tf.matmul(self.activation.read(l-1)[n], self.h_weights[l][n]) + self.bias[l]) for n in range(self.n_neurons)])
+        self.activation.write(self.n_layers-1, [tf.math.sigmoid(tf.matmul(self.activation.read(self.n_layers-2)[n], self.h_weights[self.n_layers-1][n]) + self.bias[self.n_layers-1]) for n in range(self.n_neurons)])
 
 
-            mean_weights = tf.reduce_mean(self.h_weights[self.n_layers-1], axis=0)
-            mean_activation = tf.reduce_mean(tf.reduce_mean(self.activation.read(self.n_layers-1), axis=0))
+        mean_weights = tf.reduce_mean(self.h_weights[self.n_layers-1], axis=0)
+        mean_activation = tf.reduce_mean(self.activation.read(self.n_layers-1), axis=1)
 
-            self.predicted = tf.math.sigmoid(tf.matmul(mean_activation.reshape(-1, 1), tf.transpose(mean_weights.reshape(-1, 1))) + self.bias[self.n_layers-1]).reshape(-1, 1)
-            #self.loss = lambda : 1/2 * tf.reduce_sum( tf.square((self.predicted - self.y)))
-            self.loss = lambda : tf.losses.mean_squared_error(self.predicted, y)
-            self.learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(self.lr, 5, 0.85, name="Learning_Rate")
-            self.optimizer = tf.compat.v1.train.AdamOptimizer(self.learning_rate).minimize(self.loss, var_list=[self.f_weights, self.h_weights, self.bias], name="Optimizer")
-            self.corrects = tf.equal(self.predicted, y)
-            self.accuracy = tf.reduce_mean(tf.cast(self.corrects, tf.float32))
-            
-            print(self.loss)
-            print(self.corrects)
-            print("....-------------------------")
-            print(self.predicted)
-            print("-------------------------------")
-            print(y)
+        self.predicted = tf.reduce_max(tf.math.sigmoid(tf.matmul(mean_activation, mean_weights) + tf.reshape(self.bias[self.n_layers-1], [-1, 1])), axis=0)
+        self.loss = lambda : 1/2 * tf.reduce_sum( tf.square((tf.reshape(self.predicted, [-1, 1]) - self.y)))
+        #self.loss = lambda : tf.losses.mean_squared_error(self.predicted, y)
+        self.learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(self.lr, 5, 0.85, name="Learning_Rate")
+        #self.optimizer = tf.compat.v1.train.AdamOptimizer(self.learning_rate).minimize(self.loss, var_list=[self.f_weights, self.h_weights, self.bias], name="Optimizer")
+        self.optimizer = tf.optimizers.Adam(self.learning_rate).minimize(self.loss, var_list=[self.f_weights, self.h_weights, self.bias], name="Optimizer")
 
-            self.initialized = True
+        self.corrects = tf.equal(self.predicted, y)
+        self.accuracy = tf.reduce_mean(tf.cast(self.corrects, tf.float32))
+
+        self.initialized = True
 
     def train(self):
         if self.initialized:
