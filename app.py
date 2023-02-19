c6120e66d8aebfe80c1b58e857245be1c353329a
Peter Bruegel <masterammo01@gmail.com>
2023-02-19 16:47:54 +0100

    gg

--- app.py
+++ app.py
@@ -19,17 +19,22 @@ DXFEED_URL = 'demo.dxfeed.com:7300'
 INDEX_STANDARD = colored(Graphics.INDEX_STANDARD, 'green', attrs=["reverse"])
 
 def main():
-    connection = dxc.dxf_create_connection(DXFEED_URL)
-    connection_print = colored("Connected", 'green')
-    print(f"{INDEX_STANDARD} connection: {DXFEED_URL} is {connection_print}")
+    #connection = dxc.dxf_create_connection(DXFEED_URL)
+    #connection_print = colored("Connected", 'green')
+    connection = dx.Endpoint(DXFEED_URL)
+    print(f"{INDEX_STANDARD} connection: {DXFEED_URL} is {connection.connection_status}")
 
+    sub = connection.create_subscription(event_type='Quote')
+    trade_sub = sub.add_symbols(['C', 'IBM'])
+    quote_handler = trade_sub.get_event_handler()
+    print(quote_handler.get_dataframe().head(3))
     #sub = connection.dxf_create_subscription(connection, 'Quote')
-    sub = dxc.dxf_create_subscription_timed(connection, 'Quote', int((datetime.now() - relativedelta(days=3)).timestamp()))
-    handler = DefaultHandler()
-    sub.set_event_handler(handler)
-    dxc.dxf_attach_listener(sub)
-    dxc.dxf_add_symbols(sub, ['AAPL'])
-    handler.get_list()[-5:]
+    #sub = dxc.dxf_create_subscription_timed(connection, 'Quote', int((datetime.now() - relativedelta(days=3)).timestamp()))
+    #handler = DefaultHandler()
+    #sub.set_event_handler(handler)
+    #dxc.dxf_attach_listener(sub)
+    #dxc.dxf_add_symbols(sub, ['AAPL'])
+    #andler.get_list()[-5:]
 
 
 if __name__ == '__main__':
